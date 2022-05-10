ebe2c5f05c4d3b4ccb364f860dc0d5f509710b8c
Zach <zdyar89@gmail.com>
2022-05-09 20:47:41 -0400

    Added endpoint code

--- main.go
+++ main.go
@@ -1,11 +1,44 @@
 package main
 
 import(
-	"fmt",
-	"log",
+	"fmt"
+	"log"
 	"net/http"
 )
 
-func main{
-	
+func formHandler(w http.ResponseWriter, r *http.Request){
+	if err := r.ParseForm(); err != nil (
+		fmt.fprint(w, "ParseForm() err: %v", err)
+		return
+	)
+	fmt.fprint(w, "POST request successful")
+	name := r.formValue("name")
+	address := r.formValue("address")
+	fmt.fprint(w, "Name = %s\n", name)
+	fmt.fprint(w, "Address = %s\n", address)
+}
+
+func helloHandler(w http.ResponseWriter, r *http.Request)(
+	if r.URL.Path != "/hello" (
+		http.Error(w, "404 not found", http.StatusNotFound)
+		return
+	)
+
+	if r.Method != "GET"(
+		http.Error(w, "method is not supported", http.StatusNotFound)
+		return
+	)
+
+	fmt.fprint(w, "hello!")
+
+	func main{
+		fileServer := http.FileServer(http.Dir("./static"))
+		http.Handle("/", fileServer)
+		http.HandleFunc("/form", formHandler)
+		http.HandleFunc("/hello", helloHandler)
+
+		fmt.Print("Starting server at port 8080\n")
+		if err := http.ListenAndServer(":8080", nil); err !=nill (
+			log.Fatal(err)
+		)
 }
